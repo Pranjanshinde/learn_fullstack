80fe54995172cb3805b1c2ee9ff9f435fe70d62e
Pranjanshinde <pranjanshinde10@gmail.com>
2023-05-01 16:28:58 +0530

    document sfor backend

--- Routes/Notes.router.js
+++ Routes/Notes.router.js
@@ -2,11 +2,45 @@ const express=require("express");
 const { noteModel } = require("../model/Notes.model");
 const notesroute=express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *  name: notes
+ *  description: All the api related to notes
+ *
+ */
+
+/**
+ * @swagger
+ *  /notes:
+ *  get:
+ *      summary: this will get all notes data from database
+ *      tags: [notes]
+ *      responses:
+ *          200:
+ *              description: list of all notes
+ *          400:
+ *              description: Incorrect Request
+ */
+
 notesroute.get("/",async(req,res)=>{
 const users=await noteModel.find({author_id:req.body.author_id});
 res.send(users);
 
 });
+
+/**
+ * @swagger
+ *  /notes/post:
+ *  post:
+ *      summary: this will post new post to database
+ *      tags: [notes]
+ *      responses:
+ *          200:
+ *              description: posted successfully
+ *          400:
+ *              description: Incorrect Request
+ */
 
 notesroute.post("/post",async(req,res)=>{
 try {
@@ -18,6 +52,18 @@ res.send({"mag":"new note has been added"});
 }
 });
 
+/**
+ * @swagger
+ *  /notes/update/{id}:
+ *  patch:
+ *      summary: this will update the note in database
+ *      tags: [notes]
+ *      responses:
+ *          200:
+ *              description: updated successfully
+ *          400:
+ *              description: Incorrect Request
+ */
 notesroute.patch("/update/:id",async(req,res)=>{
 try {
     const {id}=req.params;
@@ -35,6 +81,18 @@ try {
 }
 });
 
+/**
+ * @swagger
+ *  /notes/delete/{id}:
+ *  delete:
+ *      summary: this will deleted the note in database
+ *      tags: [notes]
+ *      responses:
+ *          200:
+ *              description: deleted successfully
+ *          400:
+ *              description: Incorrect Request
+ */
 notesroute.delete("/delete/:id",async(req,res)=>{
     try {
         const {id}=req.params;
--- index.js
+++ index.js
@@ -7,14 +7,29 @@ var cors = require('cors');
 require('dotenv').config();
 const { auth } = require("./middleware/Authorization");
 const { notesroute } = require("./Routes/Notes.router");
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 app.use(express.json());
 app.use(cors());
 app.use("/user",userRouter);
 
-app.use(auth);
+// app.use(auth);
 
 app.use("/notes",notesroute);
 
+const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Learning API documentation',
+        version: '1.0.0',
+      },
+    },
+    apis: ['./Routes/*.js'], // files containing annotations as above
+  };
+  
+  const specs = swaggerJsdoc(options);
+  app.use('/api_docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 app.get("/movie",(req,res)=>{
 
